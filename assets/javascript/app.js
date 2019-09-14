e1f0ed7d47bca1cf0f8e9f96d469901758a1403c
BlackAce <alfredo.gon195@gmail.com>
2019-09-14 00:42:32 -0700

    added calculations for time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,6 +38,8 @@ $("#add").click(function(event) {
     destination: destination,
     time: time,
     frequency: frequency
+    
+
   });
 });
 
@@ -47,8 +49,36 @@ database.ref("/train").on("child_added",function(snapshot){
     // creat new row
     var newRow = $("<tr>");
 
-    TODO //find example of calculation from class
+    //TODO //find example of calculation from class
     // calculation for next train
+    var tFrequency = frequency;
+
+    // Time is 3:30 AM
+    var firstTime = time;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
     // creat td element 
